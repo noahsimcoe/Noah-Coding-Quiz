cf6ea7f9872c6159694c63b91aec2aa80a872a90
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-07-08 14:32:43 -0500

    added the feature of a final score being calculated after completed/failure of the quiz

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var timerElement = document.querySelector(".timer-count");
+var secondsEl = document.querySelector(".seconds");
 var startButton = document.querySelector(".start-button");
 var scoreDisplay = document.querySelector("#score");
+var finalScoreDisplay = document.querySelector("#final-score");
 var button = document.querySelectorAll(".button");
 
 var questionHolder = document.querySelector(".question-holder");
@@ -13,8 +15,8 @@ var guess;
 var timerCount;
 var timer;
 var chosenQuestion = {};
-var score = "1";
-
+var score = "0";
+var finalScore = 0;
 var questionBank = [
     {
         question: "Which language is used to build the structure of a webpage?",
@@ -23,7 +25,7 @@ var questionBank = [
         answer3: "JavaScript",
         answer4: "Python",
         correct: "HTML",
-    },
+    }/*,
     {
         question: "Which language is used to style a webpage?",
         answer1: "HTML",
@@ -71,7 +73,7 @@ var questionBank = [
         answer3: "Minimize",
         answer4: "Skew",
         correct: "Minimize",
-    },
+    },*/
 ]
 
 // main function that is kicked off that starts the timer
@@ -87,7 +89,6 @@ function userChoice () {
     answerTwo.addEventListener("click", checkAnswer);
     answerThree.addEventListener("click", checkAnswer);
     answerFour.addEventListener("click", checkAnswer);
-
 }
 
 function checkAnswer (event) {
@@ -112,12 +113,15 @@ function checkAnswer (event) {
 // this picks a random question(currently in the form of an object) from the quesiton Array
 // this also assigns the value of the question/answers to the buttons on index.html
 function renderQuestion() {
-
     // this stops the timer after you have gone through all of the questions
     if (questionBank.length === 0) {
-        console.log("Out of questions")
-        lockButton();
+        questionHolder.textContent = "Well done! You have completed all of the questions";
+        hideButton();
         clearInterval(timer);
+        createFinalScore();
+        console.log(finalScore);
+        console.log(timerCount);
+        console.log(score);
     }
     else {
         // this segment of code removes the question selected from the array
@@ -125,15 +129,18 @@ function renderQuestion() {
         chosenQuestion = questionBank[pickedQ];
         questionBank.splice(pickedQ, 1);
         console.log(questionBank);
-
         questionHolder.textContent = chosenQuestion.question;
         answerOne.textContent = chosenQuestion.answer1;
         answerTwo.textContent = chosenQuestion.answer2;
         answerThree.textContent = chosenQuestion.answer3;
         answerFour.textContent = chosenQuestion.answer4;
-
         userChoice ();
     }
+}
+
+function createFinalScore () {
+    finalScore = (timerCount + score);
+    finalScoreDisplay.textContent = finalScore;
 }
 
 // this is the timer that counts down 1 second at a time to 0
@@ -141,24 +148,24 @@ function startTimer () {
     timer = setInterval(() => {
         timerCount--;
         timerElement.textContent = timerCount;
-
         if (timerCount < 0) {
+            timerElement.textContent = "You are out of time!!!";
+            secondsEl.textContent = "";
             console.log("you lose");
             // stops the timer at 0.
-            lockButton()
+            hideButton()
+            questionHolder.textContent = "";
             clearInterval(timer);
-
         }
-
     }, 1000);
 }
 
-// used to lock the buttons if the time OR questions run out
-function lockButton () {
-    answerOne.disabled = true;
-    answerTwo.disabled = true;
-    answerThree.disabled = true;
-    answerFour.disabled = true;
+// used to hide the answers
+function hideButton () {
+    answerOne.style.visibility = "hidden";
+    answerTwo.style.visibility = "hidden";
+    answerThree.style.visibility = "hidden";
+    answerFour.style.visibility = "hidden";
 }
 
 // this makes stuff start when the start button is clicked
